a37e9aa26784c507a4872238232716e72f202aac
segmentjoninsecret <sant7224carro@gmail.com>
2026-06-06 16:50:45 +0200

    Add file

---
+++ bootloader/io.h
@@ -0,0 +1,36 @@
+// io.h
+
+#ifndef IO_H
+#define IO_H
+
+#include "types.h"
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile (
+        "outb %0, %1" :: "a"(val), "Nd"(port)
+    );
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t val;
+    __asm__ volatile ("inb %1, %0" : "=a"(val) : "Nd"(port));
+    
+    return val;
+}
+
+static inline uint16_t inw(uint16_t port)
+{
+    uint16_t val;
+    __asm__ volatile ("inw %1, %0" : "=a"(val) : "Nd"(port));
+
+    return val;
+}
+
+static inline void outw(uint16_t port, uint16_t val)
+{
+    __asm__ volatile ("outw %0, %1" :: "a"(val), "Nd"(port));
+}
+
+#endif
